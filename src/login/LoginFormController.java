953c09d9a3b61f2f1b1396952e207c104d24bcf6
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-15 21:09:54 +0530

    add LoginFormController.java

--- src/login/LoginFormController.java
+++ src/login/LoginFormController.java
@@ -1,9 +1,12 @@
-package login;
+package Login;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -11,15 +14,15 @@ import java.io.IOException;
 
 public class LoginFormController{
 
-    public Button button;
-    public TextField txtUsername;
 
     public static String userName;
+    public Button btnLogin;
+    public TextField txtUserName;
 
 
     public void userLogInOnAction(ActionEvent primaryStage) throws IOException {
-        userName=txtUsername.getText();
-        Stage stage = (Stage) txtUsername.getScene().getWindow();
+        userName=txtUserName.getText();
+        Stage stage = (Stage) txtUserName.getScene().getWindow();
         stage.close();
         Stage stage1=new Stage();
         stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../client/ClientForm.fxml"))));

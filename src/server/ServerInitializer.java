5d29a705c250af229a7f719260c32d834ae52564
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-08-15 21:10:40 +0530

    update ServerInitializer.java

--- src/server/ServerInitializer.java
+++ src/server/ServerInitializer.java
@@ -1,24 +1,35 @@
 package server;
 
+import client.ClientHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class ServerInitializer extends Application {
+public class ServerInitializer{
+
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(10001);
+        Socket accept;
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("ServerForm.fxml"))));
-        primaryStage.setTitle("Live_Chat_Application");
-//        primaryStage.getIcons().add(new Image("assets/1.png"));
-        primaryStage.show();
+        while (true) {
+            System.out.println("waiting for client");
+            accept = serverSocket.accept();
+            System.out.println("new member connected!");
+            ClientHandler clientHandler = new ClientHandler(accept, clientHandlers);
+            clientHandlers.add(clientHandler);
+            clientHandler.start();
+
+        }
+
+
     }
 }
